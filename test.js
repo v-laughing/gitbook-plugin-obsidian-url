400379fc2deac334f82c52ba01969538c36de5f5
wusongxin <3228367978@qq.com>
2021-03-27 13:38:40 +0800

    tag

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 let path = require('path');
 var content = "sawfasf![[./saf/sdfasfas.ksf]]fgugjkhg";
 
+
 new_content = content.replace(/!\[\[([^\n]+)]]/g, function (match, p1, offset, string) {
     let name = path.basename(p1);
     return `![avatar](../assets/${name})`;
